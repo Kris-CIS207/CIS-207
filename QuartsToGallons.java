917387dda3b4b5a1c342c3519a37409c0a94e2a9
Kris-CIS207 <kris.cook10@yahoo.com>
2025-03-20 00:04:54 -0400

    QuartsToGallon

---
+++ QuartsToGallons.java
@@ -0,0 +1,10 @@
+public class QuartstoGallons {
+    public static void (string [] args)
+final int Quarts _per_Gallon = 4;
+int QuartsNeeded = 18;
+int gallonsNeeded = quartsNeeded / Quarts_Per_Gallon;
+int remainingQuarts = quartsNeeded % Quarts_per_gallong
+System.out.println("A job that needs " + quartsNeeded + " quarts requires " + gallonsNeeded + " gallons plus " + remainingQuarts + " quarts.");
+}
+}
+
